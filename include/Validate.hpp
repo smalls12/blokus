92ee0c260a8d32dc78eab045bf34e8517e4cd571
smalls12 <37202992+smalls12@users.noreply.github.com>
2020-02-01 17:55:55 -0700

    Fix validation; as long as one square is connected to the corner of another, it's a valid move

--- include/Validate.hpp
+++ include/Validate.hpp
@@ -16,6 +16,9 @@
  * 1. Initial move
  * 2. Normal gameplay ( after initial move )
  * 
+ * First pass is to find the mandatory corner adjacency for a vlid move
+ * Only one square in the piece needs this dependency
+ * 
  * ========================================================= */
 
 #include "Board.hpp"
@@ -91,6 +94,124 @@ class Validate
         {
             bool valid = true;
 
+            // check corner adjacency first
+            if ( CornerAdjacency(base, overlay) )
+            {
+                // the corner adjacency has been satisfied by one of the squares in the piece
+                // verify direct adjacency
+                for (int i = 1; i < 19; i++)
+                {
+                    for (int j = 1; j < 19; j++)
+                    {
+                        // found a square containing a piece attempting to be played
+                        if ( overlay.Get( Point(i, j) ) != GridSquare::EMPTY )
+                        {
+                            /* =========================================================
+                            * Pieces ontop of other Pieces
+                            * ========================================================= */
+
+                            // see if the base is already occupied at that location
+                            if ( base.Get( Point(i, j) ) != GridSquare::EMPTY )
+                            {
+                                // set the location on the overlay to draw custom square indicating the invalid
+                                // piece location
+                                overlay.Set( Point(i, j), GridSquare::INVALID );
+                                valid = false;
+
+                                // we found a reason for this move to be invalid
+                                // move on to next square
+                                continue;
+                            }
+
+                            /* =========================================================
+                            * Invalid to have pieces of the same color directly adjacent
+                            * ========================================================= */
+
+                            // also need to check adjacencies
+                            // search for pieces of the same color invalidating the game conditions
+                            // ie. pieces of the same color can only be played on the corners
+                            int TOP_BOUNDARY = i - 1;
+                            if ( base.Get( Point(TOP_BOUNDARY, j) ) == overlay.Get( Point(i, j) ) )
+                            {
+                                // set the location on the overlay to draw custom square indicating the invalid
+                                // piece location
+                                overlay.Set( Point(i, j), GridSquare::INVALID );
+                                valid = false;
+
+                                // we found a reason for this move to be invalid
+                                // move on to next square
+                                continue;
+                            }
+
+                            int BOTTOM_BOUNDARY = i + 1;
+                            if ( base.Get( Point(BOTTOM_BOUNDARY, j) ) == overlay.Get( Point(i, j) ) )
+                            {
+                                // set the location on the overlay to draw custom square indicating the invalid
+                                // piece location
+                                overlay.Set( Point(i, j), GridSquare::INVALID );
+                                valid = false;
+
+                                // we found a reason for this move to be invalid
+                                // move on to next square
+                                continue;
+                            }
+
+                            int LEFT_BOUNDARY = j - 1;
+                            if ( base.Get( Point(i, LEFT_BOUNDARY) ) == overlay.Get( Point(i, j) ) )
+                            {
+                                // set the location on the overlay to draw custom square indicating the invalid
+                                // piece location
+                                overlay.Set( Point(i, j), GridSquare::INVALID );
+                                valid = false;
+
+                                // we found a reason for this move to be invalid
+                                // move on to next square
+                                continue;
+                            }
+
+                            int RIGHT_BOUNDARY = j + 1;
+                            if ( base.Get( Point(i, RIGHT_BOUNDARY) ) == overlay.Get( Point(i, j) ) )
+                            {
+                                // set the location on the overlay to draw custom square indicating the invalid
+                                // piece location
+                                overlay.Set( Point(i, j), GridSquare::INVALID );
+                                valid = false;
+
+                                // we found a reason for this move to be invalid
+                                // move on to next square
+                                continue;
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                // the corner adjacency was not satisfied by any squares in the piece
+                // just invalidate all squares in the piece
+                for (int i = 1; i < 19; i++)
+                {
+                    for (int j = 1; j < 19; j++)
+                    {
+                        // found a square containing a piece attempting to be played
+                        if ( overlay.Get( Point(i, j) ) != GridSquare::EMPTY )
+                        {
+                            // set the location on the overlay to draw custom square indicating the invalid
+                            // piece location
+                            overlay.Set( Point(i, j), GridSquare::INVALID );
+                        }
+                    }
+                }
+            }
+
+
+            return valid;
+        }
+
+        inline static bool CornerAdjacency(Board& base, Board& overlay)
+        {
+            bool valid = false;
+
             for (int i = 1; i < 19; i++)
             {
                 for (int j = 1; j < 19; j++)
@@ -98,120 +219,43 @@ class Validate
                     // found a square containing a piece attempting to be played
                     if ( overlay.Get( Point(i, j) ) != GridSquare::EMPTY )
                     {
-                        /* =========================================================
-                         * Pieces ontop of other Pieces
-                         * ========================================================= */
-
-                        // see if the base is already occupied at that location
-                        if ( base.Get( Point(i, j) ) != GridSquare::EMPTY )
-                        {
-                            // set the location on the overlay to draw custom square indicating the invalid
-                            // piece location
-                            overlay.Set( Point(i, j), GridSquare::INVALID );
-                            valid = false;
-
-                            // we found a reason for this move to be invalid
-                            // move on to next square
-                            continue;
-                        }
-
-                        /* =========================================================
-                         * Invalid to have pieces of the same color directly adjacent
-                         * ========================================================= */
-
-                        // also need to check adjacencies
-                        // search for pieces of the same color invalidating the game conditions
-                        // ie. pieces of the same color can only be played on the corners
                         int TOP_BOUNDARY = i - 1;
-                        if ( base.Get( Point(TOP_BOUNDARY, j) ) == overlay.Get( Point(i, j) ) )
-                        {
-                            // set the location on the overlay to draw custom square indicating the invalid
-                            // piece location
-                            overlay.Set( Point(i, j), GridSquare::INVALID );
-                            valid = false;
-
-                            // we found a reason for this move to be invalid
-                            // move on to next square
-                            continue;
-                        }
-
                         int BOTTOM_BOUNDARY = i + 1;
-                        if ( base.Get( Point(BOTTOM_BOUNDARY, j) ) == overlay.Get( Point(i, j) ) )
-                        {
-                            // set the location on the overlay to draw custom square indicating the invalid
-                            // piece location
-                            overlay.Set( Point(i, j), GridSquare::INVALID );
-                            valid = false;
-
-                            // we found a reason for this move to be invalid
-                            // move on to next square
-                            continue;
-                        }
-
                         int LEFT_BOUNDARY = j - 1;
-                        if ( base.Get( Point(i, LEFT_BOUNDARY) ) == overlay.Get( Point(i, j) ) )
-                        {
-                            // set the location on the overlay to draw custom square indicating the invalid
-                            // piece location
-                            overlay.Set( Point(i, j), GridSquare::INVALID );
-                            valid = false;
-
-                            // we found a reason for this move to be invalid
-                            // move on to next square
-                            continue;
-                        }
-
                         int RIGHT_BOUNDARY = j + 1;
-                        if ( base.Get( Point(i, RIGHT_BOUNDARY) ) == overlay.Get( Point(i, j) ) )
-                        {
-                            // set the location on the overlay to draw custom square indicating the invalid
-                            // piece location
-                            overlay.Set( Point(i, j), GridSquare::INVALID );
-                            valid = false;
-
-                            // we found a reason for this move to be invalid
-                            // move on to next square
-                            continue;
-                        }
 
                         /* =========================================================
-                         * Pieces can only be played against the corners of already
-                         * laid pieces of the same color
-                         * ========================================================= */
+                        * Pieces can only be played against the corners of already
+                        * laid pieces of the same color
+                        * ========================================================= */
 
                         if ( base.Get( Point(TOP_BOUNDARY, LEFT_BOUNDARY) ) == overlay.Get( Point(i, j) ) )
                         {
                             // we found a reason for this move to be valid
                             // move on to next square
-                            continue;
+                            return true;
                         }
 
                         if ( base.Get( Point(TOP_BOUNDARY, RIGHT_BOUNDARY) ) == overlay.Get( Point(i, j) ) )
                         {
                             // we found a reason for this move to be valid
                             // move on to next square
-                            continue;
+                            return true;
                         }
 
                         if ( base.Get( Point(BOTTOM_BOUNDARY, LEFT_BOUNDARY) ) == overlay.Get( Point(i, j) ) )
                         {
                             // we found a reason for this move to be valid
                             // move on to next square
-                            continue;
+                            return true;
                         }
 
                         if ( base.Get( Point(BOTTOM_BOUNDARY, RIGHT_BOUNDARY) ) == overlay.Get( Point(i, j) ) )
                         {
                             // we found a reason for this move to be valid
                             // move on to next square
-                            continue;
+                            return true;
                         }
-
-                        // not a valid mvoe
-                        // set the location on the overlay to draw custom square indicating the invalid
-                        // piece location
-                        overlay.Set( Point(i, j), GridSquare::INVALID );
-                        valid = false;
                     }
                 }
             }
